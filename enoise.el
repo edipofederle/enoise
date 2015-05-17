9e065be1191755db0fc152d712d08798e7841586
elf <edipofederle@gmail.com>
2015-05-17 11:47:54 -0300

    Rename buffer. Switch to enoise buffer and display the audio name

--- enoise.el
+++ enoise.el
@@ -2,9 +2,11 @@
 	"Play a mp3 file given the name. This function use afplay command
    present on MAC OSX."
 	(interactive (list (completing-read "Noise name: " (audios (directory-files "~/audios/" t "\.mp3$")))))
-	(let* (buf (get-buffer-create "playnoise"))
-		(start-process-shell-command 
-		 "play" buf (concat (format "afplay ~/audios/%s" audio) ".mp3"))))
+		(let* (buf (get-buffer-create "enoise"))
+			(switch-to-buffer "enoise")
+			(insert (format "Play: %s\n" audio))
+			(start-process-shell-command 
+			 "play" buf (concat (format "afplay ~/audios/%s" audio) ".mp3"))))
 
 (defun audios (files) 	
 	"Returns a list of names for all files (audios)"
